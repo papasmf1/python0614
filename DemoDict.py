be8cf5a768beb3e2cf4e35cc01cfd4beac5f4e66
papasmf1 <papasmf1@gmail.com>
2021-06-14 16:50:07 +0900

    딕셔너리 데모2

--- DemoDict.py
+++ DemoDict.py
@@ -17,4 +17,36 @@ color["kiwi"] = "green"
 for item in color.items():
     print(item)
 
+#삭제
+del color["apple"]
+print( color )
 
+color.clear()
+print( color )
+
+#모바일 앱 팀(장비 관리)
+device = {"아이폰":5, "아이패드":10, "윈도우":20}
+print( device["아이폰"] )
+#입력
+device["맥프레"] = 15 
+print( device )
+#수정
+device["아이폰"] = 6 
+print( device )
+#삭제
+del device["아이폰"]
+print( device )
+
+for item in device.items():
+    print(item)
+
+for k,v in device.items():
+    print(k, v)
+
+#키만 리턴 
+for k in device.keys():
+    print(k)
+
+#값만 리턴 
+for v in device.values():
+    print(v)
