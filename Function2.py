d02c15d47a70853ab28cd53c134202245ce3b9e7
papasmf1 <papasmf1@gmail.com>
2021-06-15 13:45:18 +0900

    Function3

--- Function2.py
+++ Function2.py
@@ -32,3 +32,25 @@ def union(*ar):
 #함수 호출
 print( union("HAM","EGG") )
 print( union("HAM","EGG","SPAM") )
+
+#정의되지 않은 인자 
+def userURIBuilder(server, port, **user):
+    str = "http:" + server + ":" + port + "/?"
+    for key in user.keys():
+        str += key + "=" + user[key] + "&"
+    return str 
+
+#호출
+print( userURIBuilder("credu.com", "80", id="kim", password="1234") )
+print( userURIBuilder("credu.com", "80", id="kim", password="1234", 
+    name="mike", age="30") )
+
+#람다함수 정의(간단하게 함수를 정의하는 문법)
+g = lambda x,y:x*y 
+print( g(3,4) )
+print( g(5,6) )
+#임시로 사용(즉흥적으로)
+print( (lambda x:x*x)(3) )
+
+print( globals() )
+
