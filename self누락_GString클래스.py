e108a61a4e313231b489dafd14feca0ba6af2373
papasmf1 <papasmf1@gmail.com>
2021-06-16 08:46:50 +0900

    화요일 캡쳐

---
+++ self누락_GString클래스.py
@@ -0,0 +1,16 @@
+#전역변수 
+str = "Not Class Member"
+#클래스 정의
+class GString:
+    def __init__(self):
+        #인스턴스 멤버 변수 
+        self.str = "" 
+    def set(self, msg):
+        self.str = msg
+    def print(self):
+        #약간의 버그(멤버 변수)
+        print(self.str)
+
+g = GString()
+g.set("First Message")
+g.print()
