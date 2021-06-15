5d5b96a34dd0f2f395db790d054ab54f7dfa5ac9
papasmf1 <papasmf1@gmail.com>
2021-06-15 11:33:01 +0900

    함수2

--- Function1.py
+++ Function1.py
@@ -29,3 +29,44 @@ def intersect(prelist, postlist):
 #함수 호출
 #중단점(Break Point)
 print( intersect("HAM", "SPAM") )
+
+#불변형식 
+print("---불변형식---")
+a = 1.2 
+print( id(a) )
+a = 2.3 
+print( id(a) )
+
+#가변형식
+print("---가변형식---")
+lst = [1,2,3]
+print( id(lst) )
+lst.append(4)
+print( id(lst) )
+
+#가변형식으로 입력 + 출력 
+def change(x):
+    #복사본을 만들어서 사용
+    x1 = x[:]
+    x1[0] = "H"
+    print("함수내부:", x1)
+
+#함수 호출
+wordlist = ["J","A","M"]
+change(wordlist)
+print("함수호출후:", wordlist)
+
+#함수 내부의 이름을 해석(스코핑룰): LGB(Local, Global, Built-in)
+x = 5 
+def func1(a):
+    return a+x 
+
+#호출
+print( func1(1) )
+
+def func2(a):
+    x = 2 
+    return a+x 
+
+#호출
+print( func2(1) )
