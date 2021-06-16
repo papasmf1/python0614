b3aaee7489b6a79693d476c276b45a4e263e1a4a
papasmf1 <papasmf1@gmail.com>
2021-06-16 09:47:00 +0900

    멤버 숨김

--- BankAccount.py
+++ BankAccount.py
@@ -3,18 +3,23 @@
 #은행의 계정을 표현한 클래스 
 class BankAccount:
     def __init__(self, id, name, balance):
-        self.id = id
-        self.name = name 
-        self.balance = balance 
+        #멤버변수 숨김(이름 변경)
+        self.__id = id
+        self.__name = name 
+        self.__balance = balance 
     def deposit(self, amount):
-        self.balance += amount 
+        self.__balance += amount 
     def withdraw(self, amount):
-        self.balance -= amount
+        self.__balance -= amount
+    #인스턴스의 문자열(내가 원하는 양식)
     def __str__(self):
-        return "{0} , {1} , {2}".format(self.id, \
-            self.name, self.balance)
+        return "{0} , {1} , {2}".format(self.__id, \
+            self.__name, self.__balance)
 
 #인스턴스 객체를 생성
 account1 = BankAccount(100, "전우치", 15000)
+account1.deposit(5000)
 account1.withdraw(3000)
+#멤버 변수에 접근 (이름 변경: _클래스명__변수명)
+print(account1._BankAccount__balance)
 print(account1)
--- 참조카운트_증가감소.py
+++ 참조카운트_증가감소.py
@@ -1,13 +1,14 @@
 class MyClass:
+    #생성자 메서드 
     def __init__(self, value):
         self.Value = value 
-        print("Class is created! Value = ", value)
-        
+        print("Instance is created! Value = ", value)
+    #소멸자 메서드         
     def __del__(self):
-        print("Class is deleted!")
+        print("Instance is deleted!")
         
 
 c = MyClass(10)
-c_copy = c
-del c 
-del c_copy
+#del c 
+
+print("전체 코드 실행 종료")
